0286b587aa2d07d98a087e51be143a88c453ea25
Efrain Ake <efrain.ake97@gmail.com>
2020-12-18 15:49:16 -0600

    final

--- force-app/main/default/lwc/coolCalculator/coolCalculator.js
+++ force-app/main/default/lwc/coolCalculator/coolCalculator.js
@@ -1,37 +1,223 @@
 import { LightningElement } from 'lwc';
 
 export default class CoolCalculator extends LightningElement {
-    firstOperand;
-    secondOperand;
-    _result;
-
-    handleFirstOperand(event) {
-        this.firstOperand = event.detail.value;
-        this.emitEvent();
+    firstOperand = '';
+    dataEntered = [];
+    dataEntereds = '';
+    equalpressed = false;
+    error = false;
+    result;
+    handleClickClear() {
+        this.dataEntereds = '';
+        this.dataEntered = [];
     }
-    handleSecondOperand(event) {
-        this.secondOperand = event.detail.value;
-        this.emitEvent();
-    }
-
-    get isValid(){
-        return this.firstOperand && this.secondOperand;
-    }
-
-    get result() {
-        if (this.isValid) {
-            return +this.firstOperand + +this.secondOperand;
+    handleClickOperand(event) {
+        if (this.equalpressed) {
+            this.handleClickClear();
+            this.equalpressed = false;
         }
-        return 'Llena los campos';
+        if (this.error) {
+            this.handleClickClear();
+            this.error = false;
+        }
+
+        this.firstOperand += event.target.label;
+        this.dataEntereds += event.target.label;
+    }
+    handleClickOperator(event) {
+        if (this.error) {
+            this.handleClickClear();
+            this.error = false;
+        }
+        if (this.equalpressed) {
+            this.handleClickClear();
+            this.equalpressed = false;
+        }
+        if (this.firstOperand != '') {
+            this.dataEntered.push(this.firstOperand);
+            this.firstOperand = '';
+        }
+        this.dataEntered.push(event.target.label);
+        this.dataEntereds += event.target.label;
     }
 
-    emitEvent() {
-        if (this.isValid) {
-            this.dispatchEvent(
-                new CustomEvent('notify', {
-                    detail: this.result
-                })
-            );
+    resultado() {
+        this.dataEntered.push(this.firstOperand);
+        this.firstOperand = '';
+        this.dataEntereds = '';
+        let result = '';
+
+        for (let index = 0; index < this.dataEntered.length; index++) {
+            if (this.dataEntered[this.dataEntered.length - 1] === '') {
+                this.dataEntereds = 'Sintax Error';
+                this.error = true;
+                return;
+            }
+            if (this.dataEntered[0] == '-' || this.dataEntered[0] == '+') {
+                result += this.dataEntered[0];
+                result += this.dataEntered[1];
+                this.dataEntered.splice(0, 1, result);
+                this.dataEntered.splice(1, 1);
+                result = '';
+            }
+            //
+            switch (this.dataEntered[index]) {
+                case '/':
+                    if (
+                        this.dataEntered[index - 1] == '-' ||
+                        this.dataEntered[index - 1] == '+'
+                    ) {
+                        this.dataEntereds = 'Sintax Error';
+                        this.error = true;
+                        return;
+                    }
+                    if (
+                        this.dataEntered[index + 1] == '-' ||
+                        this.dataEntered[index + 1] == '+'
+                    ) {
+                        result += this.dataEntered[index + 1];
+                        result += this.dataEntered[index + 2];
+                        this.dataEntered.splice(index + 1, 1, result);
+                        this.dataEntered.splice(index + 2, 1);
+                        result = '';
+
+                        result =
+                            +this.dataEntered[index - 1] /
+                            +this.dataEntered[index + 1];
+                        this.dataEntered.splice(index, 1, result);
+                        this.dataEntered.splice(index - 1, 1);
+                        this.dataEntered.splice(index, 1);
+                        index = index - 2;
+                        result = '';
+                    } else {
+                        result =
+                            +this.dataEntered[index - 1] /
+                            +this.dataEntered[index + 1];
+                        this.dataEntered.splice(index, 1, result);
+                        this.dataEntered.splice(index - 1, 1);
+                        this.dataEntered.splice(index, 1);
+                        index = index - 2;
+                        result = '';
+                    }
+
+                    break;
+                case 'x':
+                    if (
+                        this.dataEntered[index - 1] == '-' ||
+                        this.dataEntered[index - 1] == '+'
+                    ) {
+                        this.dataEntereds = 'Sintax Error';
+                        this.error = true;
+                        return;
+                    }
+                    if (
+                        this.dataEntered[index + 1] == '-' ||
+                        this.dataEntered[index + 1] == '+'
+                    ) {
+                        result += this.dataEntered[index + 1];
+                        result += this.dataEntered[index + 2];
+                        this.dataEntered.splice(index + 1, 1, result);
+                        this.dataEntered.splice(index + 2, 1);
+                        result = '';
+
+                        result =
+                            +this.dataEntered[index - 1] *
+                            +this.dataEntered[index + 1];
+                        this.dataEntered.splice(index, 1, result);
+                        this.dataEntered.splice(index - 1, 1);
+                        this.dataEntered.splice(index, 1);
+                        index = index - 2;
+                        result = '';
+                    } else {
+                        result =
+                            +this.dataEntered[index - 1] *
+                            +this.dataEntered[index + 1];
+                        this.dataEntered.splice(index, 1, result);
+                        this.dataEntered.splice(index - 1, 1);
+                        this.dataEntered.splice(index, 1);
+                        index = index - 2;
+                        result = '';
+                    }
+
+                    break;
+                default:
+                    break;
+            }
         }
+
+        for (let index = 0; index < this.dataEntered.length; index++) {
+            switch (this.dataEntered[index]) {
+                case '+':
+                    if (
+                        this.dataEntered[index + 1] == '-' ||
+                        this.dataEntered[index + 1] == '+'
+                    ) {
+                        result += this.dataEntered[index + 1];
+                        result += this.dataEntered[index + 2];
+                        this.dataEntered.splice(index + 1, 1, result);
+                        this.dataEntered.splice(index + 2, 1);
+                        result = '';
+
+                        result =
+                            +this.dataEntered[index - 1] +
+                            +this.dataEntered[index + 1];
+                        this.dataEntered.splice(index, 1, result);
+                        this.dataEntered.splice(index - 1, 1);
+                        this.dataEntered.splice(index, 1);
+                        index = index - 2;
+                        result = '';
+                    } else {
+                        result =
+                            +this.dataEntered[index - 1] +
+                            +this.dataEntered[index + 1];
+                        this.dataEntered.splice(index, 1, result);
+                        this.dataEntered.splice(index - 1, 1);
+                        this.dataEntered.splice(index, 1);
+                        index = index - 2;
+                        result = '';
+                    }
+                    break;
+                case '-':
+                    if (
+                        this.dataEntered[index + 1] == '-' ||
+                        this.dataEntered[index + 1] == '+'
+                    ) {
+                        result += this.dataEntered[index + 1];
+                        result += this.dataEntered[index + 2];
+                        this.dataEntered.splice(index + 1, 1, result);
+                        this.dataEntered.splice(index + 2, 1);
+                        result = '';
+
+                        result =
+                            +this.dataEntered[index - 1] -
+                            +this.dataEntered[index + 1];
+                        this.dataEntered.splice(index, 1, result);
+                        this.dataEntered.splice(index - 1, 1);
+                        this.dataEntered.splice(index, 1);
+                        index = index - 2;
+                        result = '';
+                    } else {
+                        result =
+                            +this.dataEntered[index - 1] -
+                            +this.dataEntered[index + 1];
+                        this.dataEntered.splice(index, 1, result);
+                        this.dataEntered.splice(index - 1, 1);
+                        this.dataEntered.splice(index, 1);
+                        index = index - 2;
+                        result = '';
+                    }
+
+                    break;
+                default:
+                    break;
+            }
+        }
+        if (isNaN(this.dataEntered[0])) {
+            this.dataEntereds = 'Sintax Error';
+            this.error = true;
+        } else {
+            this.dataEntereds = this.dataEntered[0].toString();
+        }
+        this.equalpressed = true;
     }
 }
